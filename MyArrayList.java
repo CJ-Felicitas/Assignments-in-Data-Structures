3dfdb9c0ca1ec9ee32718b02d7a73f397bb775c1
CJ Felicitas <cjbfelicitas@usep.edu.ph>
2021-09-28 20:48:10 +0800

    updated pero int ra ang gamit nako

--- MyArrayList.java
+++ MyArrayList.java
@@ -1,3 +1,7 @@
+import java.util.Scanner;
+
+import javax.swing.plaf.synth.SynthSplitPaneUI;
+
 public class MyArrayList <AnyType> implements Iterable<AnyType>{
     
     private static final int DEFAULT_CAPACITY=10;
@@ -44,9 +48,23 @@ public class MyArrayList <AnyType> implements Iterable<AnyType>{
         add(size(),x);
         return true;
     }
-    
+
+    public void show_list() {
+        String s;
+        s = "[";
+        if (size() > 0)
+            s += theItems[0];
+        if (size() > 1)
+        for (int i = 1; i <= size() - 1; i++) {
+            s += ", " + theItems[i];
+        }
+        System.out.println(s+"]");
+    }
+
+        
+
     public void add (int idx, AnyType x){
-        System.out.println("Array Length: "+ theItems.length+"\nSize: "+size());
+      
         if(theItems.length == size()) ensureCapacity(size()*2+1);
         for (int i = theSize; i> idx; i--) theItems[i] = theItems [i-1];
         theItems[idx]=x;
@@ -73,32 +91,122 @@ public class MyArrayList <AnyType> implements Iterable<AnyType>{
         public boolean hasNext() {return current < size();}
         public AnyType next () { return theItems[current++]; }
         public void remove () {MyArrayList.this.remove(--current);}
-        }
+        
     
-     public static void main(String args[]) {
-        
-         MyArrayList l = new MyArrayList();
-         
-         l.add('A');
-         l.add(3);
-         l.add('B');
-         l.add('C');
-         
-         for(int i=0; i<l.size();i++)
-             System.out.println("Item "+i+": "+l.get(i));
-         
-         l.add(1,6);
-         
-         System.out.println();
-         
-         for(int i=0; i<l.size();i++)
-             System.out.println("Item "+i+": "+l.get(i));
-         
-         l.remove(2);
-        
-         for(int i=0; i<l.size();i++)
-             System.out.println("Item "+i+": "+l.get(i));
+    
+
+
+      
+
+    
     }
+    
+
+
+
+     public static void main(String args[]) {
+         // scanner for data input from the user.
+         Scanner kb = new Scanner(System.in);
+         MyArrayList<Integer> list = new MyArrayList<Integer>();
+ int option;
+ char verify;
+      System.out.println();
+      System.out.println();
+      System.out.println();
+     
+       // initial data that will be inserted to the array
+         list.add(1);
+         list.add(2);
+         list.add(3);
+         list.add(4);
+         list.add(5);
+         list.add(6);
+         list.add(7);
+         list.add(8);
+         list.add(9);
+         list.add(10);
+         
+ 
+         do{
+             System.out.print("1. SHOW LIST \n2. ADD \n3. SEARCH \n4. REMOVE \n5. REPLACE \n6. EXIT \n> ");
+ 
+             option = kb.nextInt();
+             
+     
+         switch (option) {
+ 
+             // displays the list of the array or the elements of the array
+             case 1: System.out.println("\nSHOW LIST");
+                 list.show_list();
+                System.out.println();
+                System.out.println();
+ 
+                 break;
+ 
+                 case 2: System.out.println("\nADD");
+                 System.out.print("data > ");
+               int data = kb.nextInt();
+               System.out.print("at index > ");
+               int index_insert = kb.nextInt();
+
+                list.add(index_insert,data);
+                System.out.println();
+                System.out.println();
+ break; 
+ 
+ //performs a search and displays the data from the index
+             case 3: System.out.println("\nSEARCH");
+             System.out.print("data > ");
+             int item_search = kb.nextInt();
+             boolean found = false;
+
+            // performs a loop to search if an item exists in the list.
+            System.out.println();
+            for (int i = 0; i < list.size(); i++) {
+                if (item_search==list.get(i)) {
+         // prints the location of item in the list if ever the item exists in the list
+                    System.out.println(item_search+" found at index "+i);
+                    found = true;
+                }
+            }
+            if (found==false) {
+                System.out.println("The item doesn't exists on the list");
+            }
+System.out.println("\n\n");
+        
+           
+
+          
+           
+ break;
+ 
+ // removes the data from the index of the array
+ case 4: System.out.println("\nREMOVE");
+          System.out.print("index > ");
+          int index_remove = kb.nextInt();
+          list.remove(index_remove);
+ 
+          
+          break;
+ // replaces the value of the index of the array
+ 
+          case 5: System.out.println("\nREPLACE");
+      System.out.print("Replace at index > ");
+      int index_replace = kb.nextInt();
+      System.out.print("Value to be replaced > ");
+       int data_replace = kb.nextInt();
+       list.set(index_replace, data_replace);
+ System.out.println("\n");
+       break;
+ 
+          default:
+                 break;
+         }
+ 
+     } while (option != 6);
+     
+  
+    } 
         
     }
     
