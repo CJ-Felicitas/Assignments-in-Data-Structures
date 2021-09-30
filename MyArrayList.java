f3a4ad5735333fc4f7af1b70b673eef9610ddd6e
CJ Felicitas <cjbfelicitas@usep.edu.ph>
2021-09-30 18:23:27 +0800

    original code

---
+++ MyArrayList.java
@@ -0,0 +1,111 @@
+public class MyArrayList<AnyType> implements Iterable<AnyType>{
+    
+    private static final int DEFAULT_CAPACITY=10;
+    
+    private int theSize;
+    private AnyType [] theItems;
+    
+    public MyArrayList() { doClear();}
+    public void clear(){ doClear(); }
+    
+    private void doClear()
+    {
+        theSize=0;
+        ensureCapacity (DEFAULT_CAPACITY);
+    }
+    
+    public int size() {return theSize;}
+    public boolean isEmpty() { return size()==0;}
+    public void trimToSize(){ensureCapacity(size());}
+    
+    public AnyType get(int idx){
+        if( idx < 0 || idx >= size())
+            throw new ArrayIndexOutOfBoundsException();
+        return theItems[idx];
+    }
+    
+    public AnyType set (int idx, AnyType newVal){
+        if( idx < 0 || idx >= size())
+            throw new ArrayIndexOutOfBoundsException();
+        AnyType old = theItems[idx];
+        theItems[idx] = newVal;
+        return old;
+    }
+    
+    public void ensureCapacity (int newCapacity){
+        if (newCapacity < theSize)
+            return;
+        AnyType [] old = theItems;
+        theItems = (AnyType[]) new Object[newCapacity];
+        for (int i=0; i<size(); i++) theItems[i]=old[i];
+    }
+    
+    public boolean add(AnyType x){
+        add(size(),x);
+        return true;
+    }
+    
+    public void add (int idx, AnyType x){
+        System.out.println("Array Length: "+ theItems.length+"\nSize: "+size());
+        if(theItems.length == size()) ensureCapacity(size()*2+1);
+        for (int i = theSize; i> idx; i--) theItems[i] = theItems [i-1];
+        theItems[idx]=x;
+        theSize++;
+    }
+    
+    public AnyType remove(int idx){
+        AnyType removedItem = theItems[idx];
+        for (int i=idx; i<size()-1; i++) theItems[i]=theItems[i+1];
+        theSize--;
+        return removedItem;
+    }
+    
+    public java.util.Iterator<AnyType> iterator () 
+    { 
+        return new ArrayListIterator();
+    }
+    
+    private class ArrayListIterator implements java.util.Iterator<AnyType>
+    {
+        
+        private int current =0;
+              
+        public boolean hasNext() {return current < size();}
+        public AnyType next () { return theItems[current++]; }
+        public void remove () {MyArrayList.this.remove(--current);}
+        }
+    
+     public static void main(String args[]) {
+        
+         MyArrayList l = new MyArrayList();
+         
+         l.add('A');
+         l.add(3);
+         l.add('B');
+         l.add('C');
+         
+         for(int i=0; i<l.size();i++)
+             System.out.println("Item "+i+": "+l.get(i));
+         
+         l.add(1,6);
+         
+         System.out.println();
+         
+         for(int i=0; i<l.size();i++)
+             System.out.println("Item "+i+": "+l.get(i));
+         
+         l.remove(2);
+        
+         for(int i=0; i<l.size();i++)
+             System.out.println("Item "+i+": "+l.get(i));
+    }
+        
+    }
+    
+    
+    
+    
+    
+    
+
+        
