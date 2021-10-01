df470fae31b5f72e01efec2ba9951bb61b557db2
CJ Felicitas <cjbfelicitas@usep.edu.ph>
2021-10-01 13:54:32 +0800

    sample code of sorted int array

---
+++ SortCode.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+
+public class SortCode {
+    public static void main(String[] args) {
+        int[] set = {10,8,7,6,4,3,2,1};
+
+        int temp;
+        ArrayList list = new ArrayList<>();
+
+
+        for (int i = 0; i < set.length; i++) {
+            for (int j = i + 1; j < set.length; j++) {
+                if (set[i] > set[j]) { // swap elements if not in order
+                    temp = set[i];
+                    set[i] = set[j];
+                    set[j] = temp;
+                }
+            }
+        }
+   
+        for (int i = 0; i < set.length; i++) {
+            System.out.println(set[i]);
+        }
+   
+    }
+}
